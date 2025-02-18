8752c7e9cf79f7d3a1fd55f9b53cbceb7ec5184b
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2025-02-18 22:25:43 +0400

    Init

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { Plugin } from "obsidian";
+
+export default class RevelFolded extends Plugin {
+	override onload() {
+		this.addCommand({
+			id: "reveal-active-file-folded",
+			name: "Reveal active file in folded file explorer",
+			icon: "folder-search",
+			callback: () => {
+				for (const leave of this.app.workspace.getLeavesOfType(
+					"file-explorer",
+				)) {
+					if (!("tree" in leave.view)) continue;
+					leave.view.tree.setCollapseAll(true);
+				}
+				setTimeout(() => {
+					for (const leave of this.app.workspace.getLeavesOfType(
+						"file-explorer",
+					)) {
+						if (!("tree" in leave.view)) continue;
+						leave.view.tree.isAllCollapsed = false;
+					}
+				}, 0);
+				setTimeout(
+					() =>
+						this.app.commands.executeCommandById(
+							"file-explorer:reveal-active-file",
+						),
+					100,
+				);
+			},
+			hotkeys: [],
+		});
+	}
+}
