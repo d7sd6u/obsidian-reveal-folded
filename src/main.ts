ee8294fe8c36f976ddaf4fcbf832ec5dbf7773b4
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2025-03-12 18:46:52 +0400

    refactor: use checkCallback

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,37 @@ export default class RevelFolded extends Plugin {
 			id: "reveal-active-file-folded",
 			name: "Reveal active file in folded file explorer",
 			icon: "folder-search",
-			callback: () => {
-				for (const leave of this.app.workspace.getLeavesOfType(
-					"file-explorer",
-				)) {
-					if (!("tree" in leave.view)) continue;
-					leave.view.tree.setCollapseAll(true);
-				}
-				setTimeout(() => {
-					for (const leave of this.app.workspace.getLeavesOfType(
-						"file-explorer",
-					)) {
-						if (!("tree" in leave.view)) continue;
-						leave.view.tree.isAllCollapsed = false;
-					}
-				}, 0);
-				setTimeout(
-					() =>
-						this.app.commands.executeCommandById(
-							"file-explorer:reveal-active-file",
-						),
-					100,
-				);
+			checkCallback: (checking) => {
+				const hasActiveFile = !!this.app.workspace.getActiveFile();
+
+				if (!checking && hasActiveFile) this.doCommand();
+
+				return hasActiveFile;
 			},
 		});
+	}
+
+	private doCommand() {
+		for (const leave of this.app.workspace.getLeavesOfType(
+			"file-explorer",
+		)) {
+			if (!("tree" in leave.view)) continue;
+			leave.view.tree.setCollapseAll(true);
+		}
+		setTimeout(() => {
+			for (const leave of this.app.workspace.getLeavesOfType(
+				"file-explorer",
+			)) {
+				if (!("tree" in leave.view)) continue;
+				leave.view.tree.isAllCollapsed = false;
+			}
+		}, 0);
+		setTimeout(
+			() =>
+				this.app.commands.executeCommandById(
+					"file-explorer:reveal-active-file",
+				),
+			100,
+		);
 	}
 }
